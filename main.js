e603785977109c1c60c3a1b9db84a97316db218a
김진영 <53967482+jin0kim326@users.noreply.github.com>
2020-12-30 14:00:59 +0900

    JS : Home ContactBtn

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 "use strict";
 
+// navbar 높이만큼 스크롤시 투명하게 만들기
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.clientHeight;
-
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
@@ -13,15 +13,25 @@ document.addEventListener("scroll", () => {
 
 // navbar메뉴를 클릭했을때 스크롤링 되게 하기
 const navbarMenu = document.querySelector(".navbar__menu");
-
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
-
   if (link == null) {
     return;
   }
+  //   const scrollTo = document.querySelector(link);
+  //   scrollTo.scrollIntoView({ behavior: "smooth" });
 
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+// home 'contact me' 버튼 클릭시 contact섹션으로 이동
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", (event) => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
